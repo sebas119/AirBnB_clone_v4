434c658bf3423e8f1e648caed9011408c09b7aa6
Sebastian Lopez H <lopez.sebastian@correounivalle.edu.co>
2020-05-16 18:01:19 +0000

    Finish with css and testing the backend

---
+++ web_dynamic/static/scripts/2-hbnb.js
@@ -0,0 +1,29 @@
+$(document).ready(function () {
+  const amenities = {};
+
+  function setAmenities(dataId, dataName) {
+    amenities[dataId] = dataName;
+  }
+
+  function delAmenity(dataId) {
+    delete amenities[dataId];
+  }
+
+  $('.amenities .popover ul li input').on('click', function () {
+    let dataId, dataName;
+    if ($(this).prop('checked') === true) {
+      dataId = $(this).attr('data-id');
+      dataName = $(this).attr('data-name');
+      setAmenities(dataId, dataName);
+    } else {
+      dataId = $(this).attr('data-id');
+      delAmenity(dataId);
+    }
+    $('.amenities h4').text($.map(amenities, (val) => val).join(', '));
+  });
+
+  $.getJSON('http://0.0.0.0:5001/api/v1/status/', function (data, textStatus, jqXHR) {
+    alert('status: ' + textStatus + ', data:' + data);
+  });
+
+});
